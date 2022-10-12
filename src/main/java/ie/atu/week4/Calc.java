7deca8a3506bf673054d70ea9ae2453400c05502
Richard Stephen <G00382880@atu.ie>
2022-10-12 10:26:06 +0100

    Added new method called divide

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,7 +1,22 @@
 package ie.atu.week4;
+import java.util.Scanner;
 
 public class Calc {
     public static void main( String[] args ) {
-
+      divide();
     }
+
+    public static void divide()
+    {
+        System.out.println("Please enter yur first number :");
+        Scanner inputs = new Scanner(System.in);
+        float firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter yur second number :");
+        float secondNumber = inputs.nextInt();
+
+        float total = firstNumber/secondNumber;
+        System.out.println("The total is " + total);
+    }
+
 }
